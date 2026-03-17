b3a11dc6339d1e4bfaa11ce33f2e86af28b8d039
lahiruwijesingheOC <lahiruwijesingheOC@users.noreply.github.com>
2026-03-17 11:19:26 +0000

    DFI-1706 page creation

--- src/test/scala/uk/gov/hmrc/ui/pages/BasePage.scala
+++ src/test/scala/uk/gov/hmrc/ui/pages/BasePage.scala
@@ -16,6 +16,76 @@
 
 package uk.gov.hmrc.ui.pages
 
+import org.openqa.selenium.support.ui.{ExpectedConditions, FluentWait, Wait}
+import org.openqa.selenium.{By, WebDriver}
+import org.scalatest.matchers.should.Matchers
 import uk.gov.hmrc.selenium.component.PageObject
+import uk.gov.hmrc.selenium.webdriver.Driver
+import uk.gov.hmrc.ui.conf.TestConfiguration
 
-trait BasePage extends PageObject {}
+import java.time.Duration
+
+trait BasePage extends PageObject with Matchers {
+  val pageUrl: String
+  val baseUrl: String           = TestConfiguration.url("disa-registration-frontend")
+  val signInButtonClassName: By = By.partialLinkText("Sign in")
+  val saveAndContinueButton: By = By.xpath("//button[contains(text(),'Save and continue')]")
+  val continueButton: By        = By.xpath("//a[contains(text(),'Continue')]")
+  val signOutButton: By         = By.xpath("//a[contains(text(),'Sign out')]")
+  val pageHeader: By            = By.xpath("//h1")
+
+  def verifyPageUrl(): Boolean =
+    getCurrentUrl == pageUrl
+
+  def verifyPageTitle(pageTitle: String, url: String): Boolean = {
+    verifyPageLoaded(url)
+    val actualTitle = getTitle
+    if (actualTitle != pageTitle) {
+      println(s"[Title Mismatch] Expected: '$pageTitle' | Actual: '$actualTitle'")
+      false
+    } else {
+      true
+    }
+  }
+
+  def verifyPageLoadedWithHeader(pageHeaderText: String, url: String): Boolean = {
+    verifyPageLoaded(url)
+    val actualHeader = getText(pageHeader)
+    if (actualHeader != pageHeaderText) {
+      println(s"[Header Mismatch] Expected: '$pageHeaderText' | Actual: '$actualHeader'")
+      false
+    } else {
+      true
+    }
+  }
+
+  private def fluentWait: Wait[WebDriver] = new FluentWait[WebDriver](Driver.instance)
+    .withTimeout(Duration.ofSeconds(2))
+    .pollingEvery(Duration.ofMillis(200))
+
+  def verifyPageLoaded(url: String = this.pageUrl): Unit = fluentWait.until(ExpectedConditions.urlToBe(url))
+
+  def navigateTo(url: String): Unit = {
+    Driver.instance.get(url)
+    verifyPageLoaded(url)
+  }
+
+  def goTo(page: BasePage): Unit = navigateTo(page.pageUrl)
+
+  def clickOnByPartialLinkText(partialLinkText: By): Unit = {
+    verifyPageLoaded()
+    click(partialLinkText)
+  }
+
+  def isElementPresent(locator: By): Boolean =
+    Driver.instance.findElements(locator).size() > 0
+
+  def clickSaveAndContinue(): Unit =
+    click(saveAndContinueButton)
+
+  def clickContinue(): Unit =
+    click(continueButton)
+
+  def signOut(): Unit =
+    click(signOutButton)
+}
